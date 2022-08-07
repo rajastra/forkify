da40cc4ad0ad0adc96819d0828aad00259bbef68
Raja Sa <rajasaputeraxii.ipa5@gmail.com>
2022-08-07 08:06:52 +0700

    refactoring code

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,13 +1,30 @@
+import * as model from './model.js'
+import recipeView from './views/recipeView.js'
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 const recipeContainer = document.querySelector('.recipe');
-
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
-// https://forkify-api.herokuapp.com/v2
+  // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+const controlRecipe = async function() {
+  try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    // loading recipe
+    await model.loadRecipe(id);
+
+    // rendering recipe
+    recipeView.render(model.state.recipe);
+
+  } catch (e) {
+    alert(e);
+  }
+}
+controlRecipe();
+
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, controlRecipe));
